b5f33283bb88696d429a638a7dcc9f041a88c967
Nguyen-Du-Lap <20130302@st.hcmuaf.edu.vn>
2024-04-22 21:14:25 +0700

    done page examining

--- frontend-reactjs/gui-user/src/pages/examing/Examining.js
+++ frontend-reactjs/gui-user/src/pages/examing/Examining.js
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from "react";
 import "./Examining.scss";
 import Header from "../../components/header/Header";
 import { Stack } from "@mui/material";
-import { Button } from "react-bootstrap";
+import { Button, CloseButton } from "react-bootstrap";
 import ListQuestion from "../../components/listQuestion/ListQuestion";
 import ListBtnQuestion from "../../components/listBtnQuestion/ListBtnQuestion";
-import { dataListQuestion } from "../../data/SampleData";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 function Examining() {
   const [move, setMove] = useState(false);
+  const [part, setPart] = useState(false);
   useEffect(() => {
     const handleScroll = () => {
       window.scrollY > 32 ? setMove(true) : setMove(false);
@@ -17,7 +19,26 @@ function Examining() {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
-
+  const handleHidden = () => {
+    setPart(!part);
+  };
+  const MySwal = withReactContent(Swal);
+  const handleSubmit = () => {
+    MySwal.fire({
+      title: "Xác nhận nộp bài",
+      text: "Bạn có chắc chắn muốn nộp bài không?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Đồng ý",
+      cancelButtonText: "Hủy",
+      reverseButtons: true,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // Xử lý logic khi người dùng đồng ý nộp bài
+        console.log("Đã nộp bài");
+      }
+    });
+  };
   return (
     <>
       <Header />
@@ -29,6 +50,9 @@ function Examining() {
           </h1>
           <Button className="btn-custom">Thoát</Button>
         </div>
+        <i className="note-top">
+          Chú ý: bạn có thể click vào Part sau đó chọn câu mà bạn muốn làm.
+        </i>
         <div className="wrap-content">
           <div className="container-left">
             <div className="content">
@@ -40,7 +64,9 @@ function Examining() {
               <div className="content">
                 <h3 className="text-time">Thời gian làm bài:</h3>
                 <span id="timeelapsed">14:30</span>
-                <Button className="btn-submit">NỘP BÀI</Button>
+                <Button className="btn-submit" onClick={handleSubmit}>
+                  NỘP BÀI
+                </Button>
                 <i className="note">
                   Chú ý: bạn có thể click vào số thứ tự câu hỏi trong bài để
                   đánh dấu review
@@ -52,7 +78,26 @@ function Examining() {
           </div>
         </div>
         <div className="contaner-bottom">
-          Nội dung ghi ở đây
+          <div className="content">
+            <h3 className="text-time">Thời gian làm bài:</h3>
+            <span id="timeelapsed">14:30</span>
+            <Button className="btn-submit" onClick={handleSubmit}>
+              NỘP BÀI
+            </Button>
+          </div>
+        </div>
+        <div
+          className={`center-part ${part && "d-none"}`}
+          onClick={() => handleHidden()}
+        >
+          Part
+        </div>
+        <div className={`center-part-content ${part || "d-none"}`}>
+          <div className="wrap-part">
+            <strong>Part</strong>
+            <CloseButton onClick={() => handleHidden()} />
+          </div>
+          <ListBtnQuestion />
         </div>
       </div>
     </>
