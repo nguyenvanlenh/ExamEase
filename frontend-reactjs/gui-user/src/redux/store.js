5ac461d61dbc8637de529953604fbef5e717f207
Nguyen-Du-Lap <20130302@st.hcmuaf.edu.vn>
2024-05-22 13:32:39 +0700

    merge code into dev

--- frontend-reactjs/gui-user/src/redux/store.js
+++ frontend-reactjs/gui-user/src/redux/store.js
@@ -6,7 +6,7 @@ import listQuestionSlice from './slices/listQuestionSlice'
 export const store = configureStore({
     reducer: {
         todos: todoSlice.reducer,
-        listQuestion: listQuestionSlice.reducer
+        listQuestion: listQuestionSlice.reducer,
         exams: examSlice.reducer
     },
 })
